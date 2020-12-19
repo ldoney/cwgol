ed3b52205ee8051b74863e3844b81761448508ce
Lincoln Doney <lincoln.doney@base2engineering.com>
2020-12-19 16:27:57 -0500

    Finished v1.0

--- src/cwgol.c
+++ src/cwgol.c
@@ -9,16 +9,15 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
+#include <errno.h>
 
-#define abs(n) ((n < 0) ? -n : n)
-#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
-#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
-#define PI 3.14159265
-
-#define DELAY 30000
+#define DELAY 60000
+#define HELPFILE "helpfile"
+#define BUFF_SIZE 100
 
 #define ICON "#"
 
+/* Conditions in which cells can be in */
 enum CONDITION {
   ALIVE,
   DEAD,
@@ -26,103 +25,157 @@ enum CONDITION {
   DYING
 };
 
-void get_condition(enum CONDITION cells[*][*], 
+/* FUNCTION PROTOTYPES */
+void update_condition(enum CONDITION cells[*][*], 
                         unsigned int x, unsigned int y); 
 
-int max_x = 0, max_y = 0;
+void initialize_board(enum CONDITION cells[*][*],
+						unsigned odds_alive,
+						unsigned seed);
+
+/* GLOBAL VARIABLES */
+unsigned max_x = 0, max_y = 0;
 int main(int argc, char *argv[]) {
+  int seed = time(0), odds = 80;
+  int *arg_pointer = NULL;
+  unsigned i, j;
+
+
+  /* Loop through arguments */
+  for (i = 1; i < argc; i++) {
+
+	/* Print out help page */
+	if(strcmp(argv[i], "-h") == 0 ||
+			strcmp(argv[i], "--help") == 0) {
+		FILE *hstream;
+		char buff[BUFF_SIZE];
+		hstream = fopen(HELPFILE, "r");
+		if (hstream == NULL) {
+			fprintf(stderr, "Error opening file: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		while(fgets(buff, BUFF_SIZE, hstream) != NULL) {
+			printf(buff);
+		}
+		fclose(hstream);
+		exit(EXIT_SUCCESS);
+	}
+
+	/* Point to seed */
+	else if (strcmp(argv[i], "-s") == 0)
+		arg_pointer = &seed;
+
+	/* Point to odds */
+	else if (strcmp(argv[i], "-p") == 0)
+		arg_pointer = &odds;
+
+	/* Load argument into program */
+	else if (arg_pointer != NULL) {
+		sscanf(argv[i], "%d", arg_pointer);
+		arg_pointer = NULL;
+	}	
+	
+	/* User inputted unrecognized input */	
+	else {
+		printf("Error: Unrecognized input %s, Quitting...\n", argv[i]);
+		exit(EXIT_FAILURE);
+	}
+  }
+
+  /* Set up ncurses */
   initscr();
   noecho();
-	curs_set(FALSE);
-
+  curs_set(FALSE);
   getmaxyx(stdscr, max_y, max_x);
 
-  char buff[50];
+  /* Create board */
   enum CONDITION cells[max_x][max_y];
+  initialize_board(cells, odds, seed);
+  
+  /* Continue until CTRL+C */
+  while (true) {
+    
+	/* Update conditions of cells */
+	for(i = 0; i < max_x; i++) {
+      for(j = 0; j < max_y; j++) {
+        update_condition(cells, i, j);
+      }
+    }
+
+	/* Clear the board */
+    erase();
+    for(i = 0; i < max_x; i++) {
+      for(j = 0; j < max_y; j++) {
+
+		/* Make dying cells dead */
+        if(cells[i][j] == DYING)
+          cells[i][j] = DEAD;
+
+		/* Print alive cells */
+		else if(cells[i][j] == ALIVE)
+          mvprintw(j, i, ICON);
+        
+		/* Print cells that are being
+		 * revived */
+		else if(cells[i][j] == RESURRECT) {
+          mvprintw(j, i, ICON);
+          cells[i][j] = ALIVE;
+        }
+      }
+    }
+	/* Refresh the board */
+    refresh();
+    usleep(DELAY);
+  }
+
+  /* Clear the board (Technically this is never called) */
+  endwin();
+  exit(EXIT_SUCCESS);
+}
+
+/* Initializes all the cells on the board */
+void initialize_board(enum CONDITION cells[max_x][max_y],
+						unsigned odds_alive,
+						unsigned seed) {
   int i, j;
+
   /* Set up random-ness */
-  srand((unsigned) time(0));
+  srand(seed);
 
   /* Spawn initial creatures */
   for (i = 0; i < max_x; i++) {
     for (j = 0; j < max_y; j++) {
-      if ((rand() % 100) < 80) {
+
+	  /* Randomize based on seed */
+      if ((rand() % 100) < odds_alive) {
         cells[i][j] = DEAD;
       } else {
         cells[i][j] == ALIVE;
       }
     }
   }
-  
-  while (true) {
-    erase();
-    for(i = 0; i < max_x; i++) {
-      for(j = 0; j < max_y; j++) {
-        get_condition(cells, i, j);
-      }
-    }
-
-    for(i = 0; i < max_x; i++) {
-      for(j = 0; j < max_y; j++) {
-        if(cells[i][j] == DYING)
-          cells[i][j] = DEAD;
-
-        if(cells[i][j] == ALIVE) {
-          mvprintw(j, i, ICON);
-        }
-        
-        if(cells[i][j] == RESURRECT) {
-          mvprintw(j, i, ICON);
-          cells[i][j] = ALIVE;
-        }
-      }
-    }
-    refresh();
-    usleep(DELAY);
-  }
-
-  endwin();
 }
 
-void get_condition(enum CONDITION cells[max_x][max_y], 
+/* Updates the condition of each cell */
+void update_condition(enum CONDITION cells[max_x][max_y], 
                         unsigned int x, unsigned int y) {
   unsigned short alive_count = 0;
-
+  int i = 0, j = 0;
+  
   /* Get count of neighbors */
-  /* On the left */
-  if (x > 0 && (cells[x-1][y] == ALIVE || cells[x-1][y] == DYING))
-    alive_count++;
-
-  /* On the right */
-  if (x < max_x - 1 && (cells[x+1][y] == ALIVE || cells[x+1][y] == DYING))
-    alive_count++;
-
-  /* On top */
-  if (y < 0 && (cells[x][y-1] == ALIVE || cells[x][y-1] == DYING))
-    alive_count++;
-
-  /* On bottom */
-  if (y < max_y - 1 && (cells[x][y+1] == ALIVE || cells[x][y+1] == DYING))
-    alive_count++;
-
-  /* Top left */
-  if ((y > 0 && x > 0) && (cells[x-1][y-1] == ALIVE || cells[x-1][y-1] == DYING))
-    alive_count++;
+  for(i = x-1; i <= x+1; i++) {
+	for(j = y-1; j <= y+1; j++) {
+		/* Check each corner */
+		if ((i >= 0 && j >= 0)
+				&& (i < max_x && j < max_y)
+				&& !(i == x && j == y) 
+				&& (cells[i][j] == ALIVE || cells[i][j] == DYING))
+			alive_count++;
+	}
+  }
   
-  /* Bottom left */
-  if ((y < max_y && x > 0) && (cells[x-1][y+1] == ALIVE || cells[x-1][y+1] == DYING))
-    alive_count++;
-  
-  /* Top right */
-  if ((y > 0 && x < max_x - 1) && (cells[x+1][y-1] == ALIVE || cells[x+1][y-1] == DYING))
-    alive_count++;
-  
-  /* Bottom right */
-  if ((cells[x+1][y+1] == ALIVE || cells[x+1][y+1] == DYING))
-    alive_count++;
-
   /* Three live neighbors survives (if dead)*/
-  if (alive_count == 3 && cells[x][y] == DEAD)
+  if (cells[x][y] == DEAD && alive_count == 3)
     cells[x][y] = RESURRECT;
   
   /* Fewer than two neighbours or more than two die */
